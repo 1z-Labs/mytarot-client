4104cc691c2c41d85cf458c4d1e539c3b5cfe660
rlagkdms <ithaeun@naver.com>
2024-09-05 17:08:15 +0900

    Style: 폰트 설정

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,17 @@
 </script>
 
 <style>
+@font-face {
+  font-family: 'SUIT-Regular';
+  src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_suit@1.0/SUIT-Regular.woff2') format('woff2');
+  font-weight: normal;
+  font-style: normal;
+}
 *{
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-  font-family: SUIT;
+  font-family:'SUIT-Regular';
   font-size: 14px;
   line-height: 16.8px;
 }
