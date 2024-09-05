60516857c98ef628a77d7341cb93eb9a16d63032
rlagkdms <ithaeun@naver.com>
2024-09-05 17:03:59 +0900

    Style: 폰트 설정

--- src/App.vue
+++ src/App.vue
@@ -11,4 +11,17 @@
 </script>
 
 <style>
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: SUIT;
+  font-size: 14px;
+  line-height: 16.8px;
+}
+html,
+body{
+  width: 100vw;
+  height: 100vh;
+}
 </style>
