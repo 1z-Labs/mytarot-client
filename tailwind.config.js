9aba0b760aea9e2a9cc9cce8037660ae2bfe6b69
fire-irror <firenoah.m@gmail.com>
2024-09-05 16:37:50 +0900

    fix : 헤더 완성, 챕터 만드는 중

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'resultBg' : '#FFF6F7',
+        'chapterTitle' : '#121212'
+      }
+    },
   },
   plugins: [],
 }
