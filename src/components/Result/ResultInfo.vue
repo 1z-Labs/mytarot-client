97a638b08a85468ddb6e38d73b2df3525b4b07df
fire-irror <firenoah.m@gmail.com>
2024-09-05 14:31:18 +0900

    파일구조 설정

---
+++ src/components/Result/ResultInfo.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+name: "ResultInfo"
+}
+</script>
+
+<template>
+  <div>
+    <h1>result screen</h1>
+  </div>
+</template>
+
+<style module>
+h1{
+  background-color: #039be5;
+}
+
+</style>
