e7058a7c20f902fc3ae8603e0786058bebb275f9
rlagkdms <ithaeun@naver.com>
2024-09-10 15:55:40 +0900

    Style: 겹치는 구문 삭제

--- src/components/WriteInfo/BirthInput.vue
+++ src/components/WriteInfo/BirthInput.vue
@@ -34,6 +34,7 @@ export default {
     };
   },
   methods: {
+    //TODO: input을 각각 검증하기 (년 - 1900~현재 ,월 - 1~12, 일 - 1~31)
     setBolder(index) { // 포커스가 들어왔을 때 테두리 변경
       this.$refs.inputs[index].style.outline = 'none';
 
