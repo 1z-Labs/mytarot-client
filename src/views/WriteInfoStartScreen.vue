4d38c6422c55b11da50a00f76058302ef3af6bb6
rlagkdms <ithaeun@naver.com>
2024-09-10 10:09:08 +0900

    Feat: exit button 이미지 추가

---
+++ src/views/WriteInfoStartScreen.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="con">
+    <div class="body">
+      <img src="@/assets/WriteInfo/exitButton.svg" alt=""/>
+    </div>
+    <div class="footer">
+      <button>시작하기</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+
+</style>
