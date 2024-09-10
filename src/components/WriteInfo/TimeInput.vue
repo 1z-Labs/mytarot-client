a7d69d13c43e764a850e3d9f839f7d5f15f7016a
rlagkdms <ithaeun@naver.com>
2024-09-10 16:09:53 +0900

    Style : Timeinput

---
+++ src/components/WriteInfo/TimeInput.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="con">
+  <input type="time"/>
+</div>
+</template>
+<script>
+
+</script>
+<style scoped>
+.con{
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: end;
+  margin-top: 4.9vh;
+}
+  input[type="time"] {
+    width: 85.3vw;
+    height: 50px;
+    border: 2px solid #e0e0e0;
+    border-radius: 8px;
+    padding: 0 10px;
+    font-size: 16px;
+  }
+  input[type="time"]:focus {
+    border: 2px solid #242424;
+    outline : none;
+  }
+</style>
